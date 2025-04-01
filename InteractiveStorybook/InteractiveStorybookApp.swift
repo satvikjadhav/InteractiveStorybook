07bd8d4bfd2f94b2455468a99dce65c15b75b5dd
satvikjadhav <satvikj5@gmail.com>
2025-04-01 13:14:12 -0500

    Initial Commit

---
+++ InteractiveStorybook/InteractiveStorybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveStorybookApp.swift
+//  InteractiveStorybook
+//
+//  Created by Satvik  Jadhav on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveStorybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
